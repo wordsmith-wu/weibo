e68e63ff17bd02d7d24a97d7299c8af67e4d74db
wordsmith <wordsmith@foxmail.com>
2018-12-26 07:48:59 +0000

    用户注册页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
+    <title>@yield('title','Weibo App') - 塔思翻译管理系统</title>
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
