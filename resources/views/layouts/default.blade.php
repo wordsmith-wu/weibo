d42176a1d8a020d390c9573ae6ddce3c65633942
wordsmith <wordsmith@foxmail.com>
2018-12-26 11:48:28 +0000

    Updated database configuration

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Weibo App') - 塔思翻译管理系统</title>
+    <title>@yield('title','TaaS') - 塔思翻译管理系统</title>
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
