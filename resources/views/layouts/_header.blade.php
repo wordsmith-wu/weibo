16fccc11aaf920467b6912fd95f3634c0ea31676
wordsmith <wordsmith@foxmail.com>
2018-12-26 11:17:09 +0000

    中文错误提示信息

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container ">
-    <a class="navbar-brand" href="{{ route('home')}}">TaaS</a>
+    <a class="navbar-brand" href="{{ route('home')}}">塔思系统</a>
     <ul class="navbar-nav justify-content-end">
       <li class="nav-item"><a class="nav-link" href="{{ route('help')}}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
