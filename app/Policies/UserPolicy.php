071f2eac3841acef4343c73a4b210d050add1051
wordsmith <wordsmith@foxmail.com>
2018-12-29 12:40:07 +0000

    0

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,14 +7,7 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
-<<<<<<< HEAD
-    use HandlesAuthorization;
 
-    public function update(User $currentUser, User $user)
-    {
-        return $currentUser->id === $user->id;
-    }
-=======
   use HandlesAuthorization;
 
   public function update(User $currentUser, User $user)
@@ -26,5 +19,4 @@ class UserPolicy
   {
   	return $currentUser->is_admin && $currentUser->id !== $user->id;
   }
->>>>>>> user-crud
 }
