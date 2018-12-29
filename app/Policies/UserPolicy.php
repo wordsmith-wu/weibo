fa67c2abd87daad3264be6f2bdd4f207e75ac9bb
wordsmith <wordsmith@foxmail.com>
2018-12-29 12:29:17 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,10 +7,15 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
-    use HandlesAuthorization;
+  use HandlesAuthorization;
 
-    public function update(User $currentUser, User $user)
-    {
-        return $currentUser->id === $user->id;
-    }
+  public function update(User $currentUser, User $user)
+  {
+    return $currentUser->id === $user->id;
+  }
+
+  public function destroy(User $currentUser, User $user)
+  {
+  	return $currentUser->is_admin && $currentUser->id !== $user->id;
+  }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Wordsmith';
         $user->email = 'wordsmith@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
